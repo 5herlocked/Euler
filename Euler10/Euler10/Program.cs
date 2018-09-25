4f8179b7e2f5ab89588a06536822e435449c9be1
Shadow <cam.v737@gmail.com>
2018-09-25 11:58:28 -0400

    Euler 10

---
+++ Euler10/Euler10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Euler10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Int64> primeList = new List<Int64>();
+            
+            Int64 limit = 2000000;
+
+            for (Int64 i = 2; i < limit; i++)
+            {
+                if (IsPrime(i)) primeList.Add(i);
+            }
+            
+            Console.WriteLine(GetSum(primeList));
+        }
+
+        private static Int64 GetSum(List<Int64> list)
+        {
+            Int64 sum = 0;
+            
+            foreach (var n in list)
+            {
+                sum += n;
+            }
+
+            return sum;
+        }
+
+        private static bool IsPrime(Int64 number)
+        {
+            Int64 i = 2;
+
+            if (number == 2) return true;
+
+            while (i <= Math.Sqrt(number))
+            {
+                if (number % i == 0) return false;
+                i++;
+            }
+
+            return true;
+        }
+    }
+}
